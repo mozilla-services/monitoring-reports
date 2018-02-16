1407c09abe4e9cf62a15c30edab9f4d8aba07706
Brian Pitts <bpitts@mozilla.com>
2018-02-16 17:06:32 -0600

    Support building with python from docker

--- build.sh
+++ build.sh
@@ -10,7 +10,18 @@ if [[ -d lambda ]]; then
 fi
 
 mkdir lambda
-pip3 install -r requirements.txt -t lambda/
+
+PIP_CMD='pip3 install -r requirements.txt -t lambda/'
+
+if command -v pip3 >/dev/null; then
+    $PIP_CMD
+elif command -v docker >/dev/null; then
+    docker run -w /root -v $(pwd):/root python:3.6 $PIP_CMD
+else
+    echo 'You must have either python 3 or docker installed to build'
+    exit 1
+fi
+
 cp incident_report.py settings.py lambda/
 
 cd lambda
