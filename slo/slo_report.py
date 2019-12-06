9c7d633a626fc6f766f4142f7f4617ec472f6985
Brian Pitts <bpitts@mozilla.com>
2019-12-06 16:29:23 -0500

    walk all incidents only once

--- slo/slo_report.py
+++ slo/slo_report.py
@@ -7,6 +7,7 @@ import boto3
 import json
 import requests
 import settings
+import collections
 
 
 def timerange_for_report():
@@ -36,24 +37,6 @@ def incident_is_ongoing(i):
     return i['status'] != 'resolved'
 
 
-def skip_incident(i, report_day):
-    # skip ongoing incidents
-    if i['resolved_at'] is None:
-        return True
-    # skip incidents flagged as false positive in their postmortem
-    if i['postmortem_body'] and 'false positive' in i['postmortem_body'].lower(
-    ):
-        return True
-    # skip incidents that aren't for the day are configured to report on
-    resolved = datetime.strptime(i['resolved_at'], '%Y-%m-%dT%H:%M:%S.%fZ')
-    resolved = timezone('UTC').localize(resolved)
-    resolved_day = resolved.date()
-    if resolved_day == report_day:
-        return False
-    else:
-        return True
-
-
 def calculate_incident_duration(i):
     created = datetime.strptime(i['created_at'], '%Y-%m-%dT%H:%M:%S.%fZ')
     resolved = datetime.strptime(i['resolved_at'], '%Y-%m-%dT%H:%M:%S.%fZ')
@@ -61,18 +44,37 @@ def calculate_incident_duration(i):
     return delta.total_seconds()
 
 
-def find_downtimes_by_component(components, incidents, day):
+def group_incidents_by_day(incidents):
+    incidents_by_day = collections.defaultdict(list)
+    for i in incidents:
+        # skip ongoing incidents
+        if i['resolved_at'] is None:
+            continue
+        # skip incidents flagged as false positive in their postmortem
+        if i['postmortem_body'] and 'false positive' in i[
+                'postmortem_body'].lower():
+            continue
+        # skip incidents that aren't for a day we are configured to report on
+        resolved = datetime.strptime(i['resolved_at'], '%Y-%m-%dT%H:%M:%S.%fZ')
+        resolved = timezone('UTC').localize(resolved)
+        resolved_day = resolved.date()
+        if resolved_day < settings.START_DATE or resolved_day > settings.END_DATE:
+            continue
+        # passed all our tests so include in list
+        incidents_by_day[resolved_day].append(i)
+    return incidents_by_day
+
+
+def find_downtimes_by_component(components, incidents):
     downtimes_by_component = {c['name']: [] for c in components}
     for i in incidents:
-        if skip_incident(i, day):
-            continue
         duration = calculate_incident_duration(i)
         for c in i['components']:
             downtimes_by_component[c['name']].append(duration)
     return downtimes_by_component
 
 
-def generate_report(downtimes_by_component, day):
+def generate_slo_report(downtimes_by_component, day):
     rows = []
     # JSON SerDE wants timestamp to be yyyy-mm-dd hh:mm:ss[.fffffffff
     day = day.strftime('%Y-%m-%d %H:%M:%S')
@@ -97,8 +99,8 @@ def write_report(rows, output_path):
             f.write("%s\n" % json.dumps(row))
 
 
-def upload_report(output_path):
-    s3_name = "%s%s" % (settings.S3_PREFIX, path.basename(output_path))
+def upload_report(output_path, prefix):
+    s3_name = "%s%s" % (prefix, path.basename(output_path))
     s3 = boto3.client('s3')
     s3.upload_file(output_path, settings.S3_BUCKET, s3_name)
 
@@ -106,15 +108,17 @@ def upload_report(output_path):
 def lambda_handler(event, context):
     components = get_components()
     incidents = get_incidents()
+    incidents_by_day = group_incidents_by_day(incidents)
+
     for day in timerange_for_report():
         display_day = day.strftime('%Y-%m-%d')
         print('generating %s' % display_day)
-        output_path = '/tmp/%s.json' % display_day
         downtimes_by_component = find_downtimes_by_component(
-            components, incidents, day)
-        rows = generate_report(downtimes_by_component, day)
+            components, incidents_by_day[day])
+        rows = generate_slo_report(downtimes_by_component, day)
+        output_path = '/tmp/%s.json' % display_day
         write_report(rows, output_path)
-        upload_report(output_path)
+        upload_report(output_path, 'slo')
 
 
 if __name__ == '__main__':
